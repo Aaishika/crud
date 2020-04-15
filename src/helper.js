28742c97624f07b7152af4666b458d7c2a8f14e6
Aaishika Rana <33522474+Aaishika@users.noreply.github.com>
2020-04-15 21:39:00 +0530

    Create helper.js

---
+++ src/helper.js
@@ -0,0 +1,15 @@
+var helper = {
+    toUTC: function(date) {
+        return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+    },
+    now: function() {
+        return new Date();
+    },
+    nowUTC: function() {
+        return this.toUTC(this.now());
+    },
+    capitalize: function(str) {
+    	return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+}
+module.exports = helper;
